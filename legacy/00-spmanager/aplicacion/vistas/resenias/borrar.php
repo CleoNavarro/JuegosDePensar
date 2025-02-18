be1d1971776e262552f159c0179794b672e6209d
CleoNavarro <latitacleo@gmail.com>
2025-02-18 18:58:03 +0100

    proyectoCancelado

--- legacy/00-spmanager/aplicacion/vistas/resenias/borrar.php
+++ legacy/00-spmanager/aplicacion/vistas/resenias/borrar.php
@@ -22,16 +22,16 @@ echo CHTML::dibujaEtiqueta("h2", [], "Datos de la reseña:");
 
     echo CHTML::dibujaEtiqueta("div", ["class" => "datosInfo"], null, false);
 
-    echo CHTML::dibujaEtiqueta("ul", [], null, false);
-        echo CHTML::dibujaEtiqueta("li", [], "<b>Fecha de la reseña:</b> ".
-            CGeneral::fechahoraMysqlANormal($resenia->fecha));
-        echo CHTML::dibujaEtiqueta("li", [], "<b>Reseñador:</b> ".$resenia->nick_reseniador);
-        echo CHTML::dibujaEtiqueta("li", [], "<b>Nombre sitio:</b> ".$resenia->nombre_sitio);
-        echo CHTML::dibujaEtiqueta("li", [], "<b>Puntuación:</b> ".$resenia->puntuacion." sobre 5");
-        echo CHTML::dibujaEtiqueta("li", [], "<b>Título:</b> ".$resenia->titulo);
-        echo CHTML::dibujaEtiqueta("li", [], "<b>Descripción:</b> ".$resenia->descripcion);
-        echo CHTML::dibujaEtiqueta("li", [], "<b>Borrado:</b> ".$resenia->borrado);
-    echo CHTML::dibujaEtiquetaCierre("ul");
+        echo CHTML::dibujaEtiqueta("ul", [], null, false);
+            echo CHTML::dibujaEtiqueta("li", [], "<b>Fecha de la reseña:</b> ".
+                CGeneral::fechahoraMysqlANormal($resenia->fecha));
+            echo CHTML::dibujaEtiqueta("li", [], "<b>Reseñador:</b> ".$resenia->nick_reseniador);
+            echo CHTML::dibujaEtiqueta("li", [], "<b>Nombre sitio:</b> ".$resenia->nombre_sitio);
+            echo CHTML::dibujaEtiqueta("li", [], "<b>Puntuación:</b> ".$resenia->puntuacion." sobre 5");
+            echo CHTML::dibujaEtiqueta("li", [], "<b>Título:</b> ".$resenia->titulo);
+            echo CHTML::dibujaEtiqueta("li", [], "<b>Descripción:</b> ".$resenia->descripcion);
+            echo CHTML::dibujaEtiqueta("li", [], "<b>Borrado:</b> ".$resenia->borrado);
+        echo CHTML::dibujaEtiquetaCierre("ul");
 
     echo CHTML::dibujaEtiquetaCierre("div");
 
